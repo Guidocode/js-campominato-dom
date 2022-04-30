0b4a9acb4083b81a9cc95ac020b1e845de1ef43a
Guidocode <guidobuono@icloud.com>
2022-04-30 17:18:50 +0200

    Lavoro ultimato

--- js/script.js
+++ js/script.js
@@ -18,108 +18,145 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 */
 
 
-const main = document.querySelector('main');
-
-const limitBombs = 16;
-
-// Creo un evento di click sul bottone play
+const main = document.querySelector('.game-wrapper');
+const BOMBS_NUMBER = 16;
 document.getElementById('play').addEventListener('click', play);
+let bombs = [];
+let score = 0;
 
-
-// Qui scrivo cosa accade quando clicco su play 
+// Start Game
 function play(){
 
-    reset();
+  const level = document.getElementById('level').value;
+  const gridLevels = [100,81,49];
+  
+  const cellNumbers = gridLevels[level];
+  
+  reset(cellNumbers);
+  
+  // console.log('bombs', bombs);
+  
+  generateGridGame(cellNumbers);
 
-    const level = document.getElementById('level').value;
-    const gridLevels = [100, 81, 49];
-
-    const cellNumbers = gridLevels[level];
-    console.log('quantità celle', cellNumbers);
-
-    const bomb = generateBombs(cellNumbers);
-    console.log(bomb);
-
-    generateGridGame(cellNumbers);
 }
 
+// Funzione che genera la griglia di gioco
+function generateGridGame(cellNumbers){
 
-// funzione per resettare il contenuto del main
-function reset(){
-    main.innerHTML = '';
+  const grid = document.createElement('div');
+  grid.className = 'grid';
+
+  for(let i = 1; i <= cellNumbers; i++){
+
+    const cell = generateCell(i, cellNumbers);
+    grid.append(cell);
+  }
+
+  main.append(grid);
+
 }
 
+// Funzione che genera le celle
+function generateCell(cellId, cellNumbers){
+  
+  const cell = document.createElement('div');
+  cell.className = 'cell';
 
-// funzione per generare la griglia di gioco
-function generateGridGame(numeroCelle){
+  cell.classList.add('square'+cellNumbers);
+  cell.innerHTML = `<span>${cellId}</span>`;
 
-    const grid = document.createElement('div');
-    grid.className = 'gb-grid';
+  // creo la proprietà custom cellIdr per andarla a leggere al click
+  cell.cellId = cellId;
 
-    for( let i = 1; i <= numeroCelle; i++ ){
-        
-        const cell = generateCell(i, numeroCelle);
-        grid.append(cell);
+  cell.addEventListener('click', clickCell);
+
+  return cell;
+}
+
+// Funzione scatenata dal click della cella
+function clickCell(){
+  /*
+    1. contare i click
+    2. "leggere" il numero della cella
+    3. verificare se il numero è presente nell'array delle bombe
+    4. se NO aggiungere la classe clicked
+    5. se SI attivare la procedura di fine gioco :-)
+  */
+  
+  console.log(this.cellId);
+
+  if(!bombs.includes(this.cellId)){
+
+    score++;
+    this.classList.add('clicked');
+    console.log('OK', this.cellId);
+    const cells = document.getElementsByClassName('cell');
+
+    if(score === cells.length - BOMBS_NUMBER){
+      printEndGame('Hai Vinto!!!');
+    }
+  }else{
+    endGame(this);
+  }
+}
+
+// Funzione che scatena la fine del gioco 
+function endGame(el){
+
+  console.log('FINE GIOCO')
+  el.classList.add('bomb');
+  const endMsg = `<h3 class="py-3">Gioco finito, hai calpestato 1 bomba su ${BOMBS_NUMBER}! Numero di tentativi: ${score}</h3>`;
+  printEndGame(endMsg);
+}
+
+// Funzione che stampa la fine del gioco
+function printEndGame(endMsg){
+
+  document.querySelector('.endMessage').innerHTML = endMsg;
+  seeAllBombs();
+  const endGameHtmlLevel = document.createElement('div');
+  endGameHtmlLevel.className = 'endGame';
+  main.append(endGameHtmlLevel);
+}
+
+// Funzione per mostrare tutte le bombe a fine gioco
+function seeAllBombs(){
+  
+  const cells = document.getElementsByClassName('cell');
+  for(let i = 0; i < cells.length; i++){
+    if(bombs.includes(i + 1)){
+      cells[i].classList.add('bomb');
     }
 
-    main.append(grid);
+  }
+  console.log(cells);
 }
-
-
-// funzione per generare le celle della griglia
-function generateCell(cellId, totCelle){
-
-    const cell = document.createElement('div');
-    cell.className = 'cell square' + totCelle;
-    cell.innerHTML = `<span>${cellId}</span>`;
-
-    cell.addEventListener('click', clickCell);
-
-    return cell;
-}
-
-
-// funzione per aggiungere la classe clicked all'elemento cliccato
-function clickCell(){
-
-    
-    
-    this.classList.add('clicked');
-}
-
-
-/*
-    1. Genero una lista di 16 numeri casuali da 1 a cellNumber
-
-    2. I numeri non devono ripetersi.
-
-    3. L’utente clicca su una cella: se il numero è presente nella lista dei numeri 
-        generati - abbiamo calpestato una bomba - la cella si colora di rosso
-
-    4. La partita termina quando il giocatore clicca su una bomba o raggiunge il 
-        numero massimo possibile di numeri consentiti.
-*/
-
 
 // Funzione che genera le bombe
 function generateBombs(cellNumbers){
+  const generatedBombs = [];
 
-    const generatedBombs = [];
+  while(generatedBombs.length < BOMBS_NUMBER){
 
-    while( generatedBombs.length < limitBombs ){
-        const bomb = getRndInteger(1, cellNumbers)
+    const bomb = generateRandomInt(1, cellNumbers);
 
-        if( !generatedBombs.includes(bomb )){
-            generatedBombs.push(bomb);
-        }
+    if(!generatedBombs.includes(bomb)){
+      generatedBombs.push(bomb);
     }
+  }
 
-    return generatedBombs;
+  return generatedBombs;
 }
 
+// Funzione di reset
+function reset(cellNumbers){
+  
+  bombs = generateBombs(cellNumbers);
+  main.innerHTML = '';
+  document.querySelector('.endMessage').innerHTML = '';
+}
 
-
-// Funzione genera numeri random con un min e max indicato da me
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+// Funzione numeri random
+function generateRandomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
